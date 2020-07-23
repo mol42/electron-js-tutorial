fd4ace8d35d150de0006c5b7b3ec9f019695aa5a
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2020-07-23 11:33:08 -0400

    main renderer process GUI call added

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const { dialog } = require("electron");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
   // and load the index.html of the app.
   // win.loadFile('index.html');
-  win.loadFile('www/index.html');
+  win.loadFile("www/index.html");
 
   win.webContents.openDevTools();
+
+  // Renderer process içinde çağrılamayan bu kod main process içinde çalışacak
+  // örnek olması amacı ile ekledim buraya.
+  setTimeout(() => {
+    dialog.showOpenDialog({ properties: ["openFile", "multiSelections"] });
+  }, 2000);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
